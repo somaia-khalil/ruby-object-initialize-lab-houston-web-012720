b7dc9f4197a151cfa9999cc774f33a2138799b20
Somaia Khalil <sumaya_khair@live.com>
2020-02-01 17:34:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,10 @@
+require 'pry'
 class Dog 
     def initialize (dog_name, dog_breed = "Mutt")
         @name = dog_name
         @breed = dog_breed
     end
 end 
+binding.pry 
+
+a1 = Dog.new ("ha")
